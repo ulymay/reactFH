43143c6829fe5101b14a9e68ef514f759c444ce1
Ulises Lopez <ulymay@gmail.com>
2021-01-24 03:41:56 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const nombre = "Ulises";
+const apellido = "Lopez";
+
+//const nombreCompleto = nombre + " " + apellido;
+const nombreCompleto = ` ${nombre} ${apellido} `;
+
+console.log(nombreCompleto);
